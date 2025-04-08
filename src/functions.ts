53aea910c2510650e145b18af2431b4f9f9b0acc
Jules Schoonman <jules.schoonman@gmail.com>
2025-04-08 15:07:08 +0200

    removed container, changed classes
    reverted Promise.all()

--- src/functions.ts
+++ src/functions.ts
@@ -4,6 +4,7 @@ export function fetchJson(url: string, body?: any) {
   const method = body ? "POST" : "GET";
   const headers = new Headers();
   const options: RequestInit = { method, headers };
+  headers.append("Accept", "application/json");
   if (body) {
     headers.append("Content-Type", "application/json");
     options.body = JSON.stringify(body);
@@ -44,51 +45,33 @@ function createTypoResults(
   resultsUrl: string
 ) {
   return /*html*/ `
-    <div class="t3ce frame-type-lookup_results">
-      ${
-        records.length && count
-          ? count + " results"
-          : records.length
-          ? "Top 3 results"
-          : "No results"
-      }
-      <h2>${title}</h2>
-      <div class="content-container">
-        ${records.length ? records.map(createTypoRow).join("\n") : ""} 
-        <div class="t3ce frame-type-sitetud_singlebutton">
-          <a href="${resultsUrl}" class="btn btn--single align-center btn--royal_blue">
-            View all results
-          </a>
+    <div class="t3ce frame-type-gridelements_pi1">
+      <div class="grid-background--white grid-background--boxed">
+        ${
+          records.length && count
+            ? count + " results"
+            : records.length
+            ? "Top 3 results"
+            : "No results"
+        }
+        <h2>${title}</h2>
+        <div class="content-container">
+          ${records.length ? records.map(createTypoRow).join("\n") : ""} 
+          <div class="t3ce frame-type-sitetud_singlebutton">
+            <a href="${resultsUrl}" class="btn btn--single align-center btn--royal_blue">
+              View all results
+            </a>
+          </div>
         </div>
       </div>
     </div>
     `;
 }
 
-function createContainer() {
-  return /*html*/ `
-    <div class="grid-background--white grid-background--boxed">
-      <div class="row grid layout-0 grid--noPaddingBottom">
-        <div id="external-search-results" class="sm-12">
-          <!-- Content will be placed here -->
-        </div>
-      </div>
-    </div>
-  `;
-}
-
 export async function createResults(searchInput: string) {
-  const container = createContainer();
-  const row = document.getElementsByClassName("sm-12 md-6")[1];
-  if (row) {
-    // Potential security risk?
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#security_considerations
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Safely_inserting_external_content_into_a_page
-    row.innerHTML = container;
-  }
-   const resultDiv = document.getElementById("external-search-results");
+  // Get flex container
+  const grid = document.getElementsByClassName("multiRowGrid")[0];
 
-  /* Previous version by Jules
   // Add results to container
   searchProviders.map(async (provider) => {
     const records = await provider.getRecords(searchInput);
@@ -99,36 +82,16 @@ export async function createResults(searchInput: string) {
         records.count,
         provider.searchBaseUrl + searchInput
       );
+      // Create flex child
       const div = document.createElement("div");
+      div.className = "sm-6 md-6 lg-6";
+      // Potential security risk?
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#security_considerations
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Safely_inserting_external_content_into_a_page
       div.innerHTML = typoResults;
-      resultDiv?.appendChild(div);
+      if (grid) {
+        grid.appendChild(div);
+      }
     }
   });
-*/ 
-
-   // get what needs to be done...
-  const recordPromises = searchProviders.map(async (provider) => {
-    const records = await provider.getRecords(searchInput);
-    return { provider, records };
-  });
-  
-  // Wait for all queries, to all websites to resolve
-  Promise.all(recordPromises).then((results) => {
-    results.forEach(({ provider, records }) => {
-      if (records) {
-        const typoResults = createTypoResults(
-          provider.title,
-          records,
-          records.count,
-          provider.searchBaseUrl + searchInput
-        );
-        const div = document.createElement("div");
-        div.innerHTML = typoResults;
-        resultDiv?.appendChild(div);
-      }
-    });
-  });
-
-
-  
 }
